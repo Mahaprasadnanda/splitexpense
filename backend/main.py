45f8ada2b59cdb0ecab883083ff9601cd5120232
Mahaprasad Nanda <140647643+Mahaprasadnanda@users.noreply.github.com>
2026-03-06 03:58:43 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,8 @@ app.add_middleware(
         "http://localhost:5175", 
         "http://localhost:3000", 
         "http://127.0.0.1:5173", 
-        "http://127.0.0.1:5174"
+        "http://127.0.0.1:5174",
+        "https://splitexpense-mu.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
